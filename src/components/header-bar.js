0c16d145e2fd31c72f78c6ccef2e53af264e4454
Neill Somerville <neill@neillsomerville.com>
2018-09-24 22:55:06 -0700

    header-bar component, local-storage handlers

---
+++ src/components/header-bar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { connect } from "react-redux";
+import { clearAuth, info } from "../actions/auth";
+import { clearAuthToken } from "../local-storage";
+import "../index.css"
+import "./styles/header-bar.css";
+
+export class HeaderBar extends React.Component {
+  logOut() {
+    this.props.dispatch(clearAuth());
+    clearAuthToken();
+  }
+
+  render() {
+    // Only render the log out button if we are logged in
+    let logOutButton;
+    if (this.props.loggedIn) {
+      logOutButton = (
+        <button className="logout-button" onClick={() => this.logOut()}>
+          Logout
+        </button>
+      );
+    }
+
+    return (
+      <div className="header-bar">
+        <div className="header">
+          <h1 className="header-title">H1 title here</h1>
+        </div>
+        
+        </button>
+
+        {logOutButton}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  loggedIn: state.auth.currentUser !== null,
+  info: state.auth.info
+});
+
+export default connect(mapStateToProps)(HeaderBar);
